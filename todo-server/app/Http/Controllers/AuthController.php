fd55a8e686c5830411fea3a492f1bb04d2b3c455
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-12-05 22:10:17 +0200

    Backend <> Adding auth controller

---
+++ todo-server/app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        $token = Auth::attempt($credentials);
+        if (!$token) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized',
+            ]);
+        }
+
+        $user = Auth::user();
+        return response()->json([
+                'status' => 'success',
+                'user' => $user,
+                'token' => $token,
+            ]);
+
+    }
+
+}
