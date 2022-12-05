0ad27aa3f6b3ceeceb53e5a7b505550a1981cb90
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-12-05 22:26:04 +0200

    Frontend <> Adding button component

---
+++ todo-application/components/button/style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles=StyleSheet.create({
+    button:{
+        width: '100%',
+        height: 45,
+        padding: 10,
+        marginTop: 20,
+        marginBottom: 10,
+        borderRadius: 10,
+        backgroundColor: '#3c9fec'
+    },
+    text:{
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: 'white'
+    }
+})
