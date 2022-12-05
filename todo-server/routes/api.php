1701334aa13837b487e2dcd90217d81177a0dcc2
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-12-05 22:20:30 +0200

    Backend <> Adding apis routes

--- todo-server/routes/api.php
+++ todo-server/routes/api.php
@@ -3,17 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\AuthController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(["middleware" => "auth:api"], function(){
+    Route::get('/user/{id}',[UserController::class,'getUser']);
+    Route::post('/task',[TaskController::class,'addTask']);
+    Route::get('/task',[TaskController::class,'getTask']);
 });
+
+Route::post('/user',[UserController::class,'addUser']);
+Route::post('/login',[AuthController::class,'login']);
